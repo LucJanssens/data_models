771348ca0a8aee068cfd3d96e0e762bd95ed9397
Janssens <JanssensLuc@users.noreply.github.com>
2017-09-26 11:01:07 +0200

    Update PersonUtils.java

--- src/main/java/com/pelter/data/model/utils/PersonUtils.java
+++ src/main/java/com/pelter/data/model/utils/PersonUtils.java
@@ -2,6 +2,7 @@ package simple;
 
 import java.time.LocalDate;
 import java.time.Period;
+import com.pekter.data.model.Person;
 
 public class PersonUtils {
 	public static int getAge(Person person) throws NullPointerException{
