848555a252aebdbb940da911703511f552b30a97
LucJanssens <LucJanssens@users.noreply.github.com>
2019-03-04 22:28:12 +0100

    Update PersonUtils.java

--- src/main/java/com/pelter/data/model/utils/PersonUtils.java
+++ src/main/java/com/pelter/data/model/utils/PersonUtils.java
@@ -5,7 +5,7 @@ import java.time.Period;
 import com.pelter.data.model.Person;
 
 public class PersonUtils {
-	public static int getAge(Person person) throws NullPointerException{
-		return Period.between(person.getBirthDate(), LocalDate.now()).getYears();		
-	}
+  public static int getAge(Person person) throws NullPointerException{
+    return Period.between(person.getBirthDate(), LocalDate.now()).getYears();		
+  }
 }
